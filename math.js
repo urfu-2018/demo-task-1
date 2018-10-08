1b68a374a396364f53fb255c328d61c609291fc0
AndrewLoginov <andrew.loginov13@gmail.com>
2018-10-08 13:49:05 +0500

    Loginov Andrew
    add sum

--- math.js
+++ math.js
@@ -4,4 +4,9 @@ exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
+    if (arguments.length === 3) {
+        return Number(a) + Number(b) + Number(arguments[2]);
+    }
+
+    return Number(a) + Number(b);
 };
