1dab5b543b56715eb09267dc0527e1a934088a76
VA-Chusovitin <43614504+VA-Chusovitin@users.noreply.github.com>
2018-10-09 11:27:30 +0500

    Первый вариант решения

--- math.js
+++ math.js
@@ -2,6 +2,18 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b) 
+{
+    if (!a.isNaN && !b.isNaN)
+    {
+        return a+b;
+    }
 };
+
+exports.sum = function (a, b, c)
+{
+    if (!a.isNaN && !b.isNaN && !c.isNaN)
+	{
+	    return a+b+c;
+	}
+}
