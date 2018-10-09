c7a7c75108de0e1421ca4ea6f4babf63f5b6757d
VA-Chusovitin <43614504+VA-Chusovitin@users.noreply.github.com>
2018-10-09 14:02:36 +0500

    Попытка версии 8.1

--- math.js
+++ math.js
@@ -3,9 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-        return Number(a) + Number(b);
+    return Number(a) + Number(b);
 };
 
 exports.sum = function (a, b, c) {
-        return Number(a) + Number(b) + Number(c);
+    return Number(a) + Number(b) + Number(c);
 };
