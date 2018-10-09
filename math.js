f92c21ad81bbe4354dd22f58c4bad7cd130ae4b2
VA-Chusovitin <43614504+VA-Chusovitin@users.noreply.github.com>
2018-10-09 13:50:29 +0500

    Седьмой вариант решения

--- math.js
+++ math.js
@@ -3,18 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    var oneArg = parseInt(a);
-    var twoArg = parseInt(b);
-    if (!isNaN(oneArg) && !isNaN(twoArg)) {
-        return oneArg + twoArg;
-    }
+        return parseInt(a) + parseInt(b);
 };
 
 exports.sum = function (a, b, c) {
-    var oneArg = parseint(a);
-    var twoArg = parseInt(b);
-    var threeArg = parseInt(c);
-    if (!isNaN(oneArg) && !isNaN(twoArg) && !isNaN(threeArg)) {
-        return oneArg + twoArg + threeArg;
-    }
+        return parseInt(a) + parseInt(b) + parseInt(c);
 };
