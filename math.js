c3d830b3c3ed82f588f7a8f32d69b8510677a766
lenastar <lena.starkova.1998@mail.ru>
2018-10-09 23:44:28 +0300

    Finish solution

--- math.js
+++ math.js
@@ -2,6 +2,6 @@
 
 exports.isStar = true;
 
-exports.sum = function (a = 0, b = 0){
-	return parseFloat(a) + parseFloat(b);	
+exports.sum = function(a = 0, b = 0){
+    return parseFloat(a) + parseFloat(b);	
 }
