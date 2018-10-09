25926859e4514f085a11cb7dde962ef426578b3a
Anna Gromova <34894576+AnnaGromova@users.noreply.github.com>
2018-10-09 12:49:37 +0500

    Добавлена вторая версия решения

--- math.js
+++ math.js
@@ -2,6 +2,6 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b, c) {
-     return Number(a) + Number(b) + (arguments[2] ? Number(c) : 0);
+exports.sum = function (a, b, c = 0) {
+    return Number(a) + Number(b) + Number(c);
 };
