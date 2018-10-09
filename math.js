2ceddec5169833e5bd2c7ac7f085c9c5c8381a75
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2018-10-09 17:01:41 +0500

    Solve2

--- math.js
+++ math.js
@@ -3,5 +3,13 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    return parseInt(a) + parseInt(b);
+    // return parseInt(a) + parseInt(b);
+    let sum = 0;
+    for (let i in arguments) {
+        if (arguments.hasOwnProperty(i)) {
+            sum += parseInt(arguments[i]);
+        }
+    }
+
+    return sum;
 };
