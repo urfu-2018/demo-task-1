7c244efe5dbb33c1f95a490e333af90c35a21d12
Shchupakov Antony <a.shchupakov@mail.ru>
2018-10-09 11:08:13 +0500

    Parse string

--- math.js
+++ math.js
@@ -3,12 +3,10 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    let s = a + b;
+    let s = Number(a) + Number(b);
     if (typeof c !== 'undefined') {
-        s += c;
+        s += Number(c);
     }
 
     return s;
 };
-
-
