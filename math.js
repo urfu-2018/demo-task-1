b198084b24b816e0edf032d8a8c3a7e97f14c3d0
ZaoAsya <zaostrovskaya.asya@yandex.ru>
2018-10-10 00:20:42 +0500

    First trying

--- math.js
+++ math.js
@@ -2,6 +2,10 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b, c) {
+    if (c) {
+        return +a + +b + +c;
+    } else {
+        return +a + +b;
+    }
 };
