29baca92c1772a411f9a5c19c6558fb3087eb4bd
romassol <romass1998@mail.ru>
2018-10-10 21:30:55 +0500

    Added checking for missing arguments

--- math.js
+++ math.js
@@ -4,6 +4,9 @@ exports.isStar = true;
 exports.sum = sum;
 
 function sum() {
+    if (arguments.length == 0) {
+        return NaN;
+    }
     let result = 0;
     for (let i = 0; i < arguments.length; i++) {
         let number = parseInt(arguments[i], 10);
