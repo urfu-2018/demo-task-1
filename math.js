4dafc94470a5c871cf998c796bd65220190e8574
Scvairy <scvairy@gmail.com>
2018-10-10 16:16:19 +0500

    sum implemented

--- math.js
+++ math.js
@@ -2,6 +2,9 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b, c) {
+    if (c === undefined) {
+        return a + b;
+    }
+    return a + b + c;
 };
