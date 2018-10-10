68a7d1053abaadd6eb6743389c83d7890fcc1538
Jamboba <vova.kurgan@yandex.ru>
2018-10-10 23:25:25 +0500

    решил задачку

--- math.js
+++ math.js
@@ -2,6 +2,19 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
+exports.sum = function (a, b, c = 0) {
     // Реализуйте сложение в этой функции
+    for (var i = 0; i < arguments.length; i ++) {
+        if (typeof a !== 'number') {
+            a = Number(a);
+        }
+        if (typeof b !== 'number') {
+            b = Number(b);
+        }
+        if (typeof c !== 'number') {
+            c = Number(c);
+        }
+
+        return a + b + c;
+    }
 };
