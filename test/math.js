1c1a8149539219f307b36d350b3f606aa5bb7f20
ZeroNetto <Zero.Netto.o@gmail.com>
2018-10-10 14:30:32 +0500

    Update math.js

--- test/math.js
+++ test/math.js
@@ -1,13 +1,7 @@
-/* eslint-env mocha */
 'use strict';
 
-const { sum } = require('../math');
-const assert = require('assert');
+exports.isStar = true;
 
-describe('Math', function () {
-    describe('sum', function () {
-        it('should sum `1` and `2` and get `3`', function () {
-            assert.strictEqual(sum(1, 2), 3);
-        });
-    });
-});
+exports.sum = function (a, b) {
+    return a + b;
+};
