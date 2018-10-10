019ba6679e189f1e7ee5fb1ec1a3983e00cdf78a
ZeroNetto <Zero.Netto.o@gmail.com>
2018-10-10 14:45:27 +0500

    Update math.js

--- math.js
+++ math.js
@@ -6,7 +6,9 @@ exports.sum = function (...nums) {
     let sum = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
+        if (!isNaN(nums[i])) {
+            sum += nums[i];
+        }
     }
 
     return sum;
