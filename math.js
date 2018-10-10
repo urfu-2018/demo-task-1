e22f6a6213b6d29af79ed88754e752c319efab1a
romassol <romass1998@mail.ru>
2018-10-10 20:46:44 +0500

    Added sum with two arguments and with three arguments

--- math.js
+++ math.js
@@ -3,5 +3,8 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+    return a + b;
+};
+exports.sum = function (a, b, c) {
+    return a + b + c;
 };
