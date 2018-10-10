bbf183cb477a1304dcd87e325aa497cf898362f7
Averin Andrey <teris123@yandex.ru>
2018-10-10 20:46:42 +0500

    Аверин Андрей, решение демки

--- math.js
+++ math.js
@@ -2,6 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b, c) {
+    if(c === undefined) c = 0;
+    return ( +a + b + c);
 };
