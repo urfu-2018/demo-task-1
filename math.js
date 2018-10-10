689302a6db4050d57cf75c22921ed06912595f3a
ZeroNetto <Zero.Netto.o@gmail.com>
2018-10-10 14:39:52 +0500

    Update math.js

--- math.js
+++ math.js
@@ -9,3 +9,11 @@ exports.sum = function (a, b) {
 
     return a + b;
 };
+
+exports.sum = function (a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        return 0;
+    }
+
+    return a + b + c;
+};
