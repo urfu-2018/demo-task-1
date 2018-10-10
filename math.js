76d382ab02b0118bcd38922c02b1201906ae6943
Averin Andrey <teris123@yandex.ru>
2018-10-10 22:01:30 +0500

    Аверин Андрей решение демо 2

--- math.js
+++ math.js
@@ -3,6 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    if(c === undefined) c = 0;
-    return ( +a + b + c);
+    if (c === undefined) {
+        c = 0;
+    }
+
+    return Number(a + b + c);
 };
