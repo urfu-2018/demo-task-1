341fefe1521369779899415ba4b149c1361516aa
Afanasev Leonid <leo-6809@mail.ru>
2018-10-11 20:51:06 +0500

    Решение задачи

--- math.js
+++ math.js
@@ -7,10 +7,10 @@ function sum() {
     if (arguments.length === 0) {
         return NaN;
     }
-    let sum = 0;
+    let amount = 0;
     for (let i = 0; i < arguments.length; i++) {
-        sum += Number(arguments[i]);
+        amount += Number(arguments[i]);
     }
 
-    return sum;
+    return amount;
 };
