73ec9408687d2fbe1198b88701eb0e0975a9ddf6
lenastar <lena.starkova.1998@mail.ru>
2018-10-11 21:25:07 +0300

    Finally

--- math.js
+++ math.js
@@ -2,6 +2,9 @@
 
 exports.isStar = true;
 
-exports.sum = function(a = 0, b = 0){
-    return parseFloat(a) + parseFloat(b);	
-}
+exports.sum = function (a = 0, b = 0) {
+    let intA = parseInt(a);
+    let intB = parseInt(b);
+
+    return intA + intB;
+};
