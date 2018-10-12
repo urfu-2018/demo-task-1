62a4e5af6e7afc40c3c9159cacc5033a73d12818
PRIZRAKeee <gagarin12041997@Mail.ru>
2018-10-12 16:27:16 +0500

    Добавлен девятый вариант решения

--- math.js
+++ math.js
@@ -2,9 +2,9 @@
 
 exports.isStar = true;
 
-exports.sum = function () {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
+exports.sum = function (a, b) {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
         sum += arguments[i];
     }
     return sum;
