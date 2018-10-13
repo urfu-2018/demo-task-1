03859bfc77ac190af40b863756ee3a6b32a35ca5
Kuznecoff <artem98kuznecov@mail.ru>
2018-10-13 22:27:28 +0500

    реализовал сложение 3-х чисел

--- math.js
+++ math.js
@@ -2,6 +2,6 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b, c=0) {
+    return Number(a) + Number(b) + Number(c);
 };
