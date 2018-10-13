b65f082dd04ee092af8847d379c2d089e3c9487f
Krivedina <azakov31@gmail.com>
2018-10-13 13:20:55 +0500

    Update math.js

--- math.js
+++ math.js
@@ -2,7 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
+exports.sum = function (a, b, c) {
     let s = Number(a) + Number(b);
     if (typeof c !== 'undefined') {
         s += Number(c);
