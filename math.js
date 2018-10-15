f1e462abd4387abd09c12bb38ef7bb5eb5f182ca
RA.Efremenkov <ra.dir@mail.ru>
2018-10-15 19:39:02 +0500

    Добавлен 2-ый вариант решения

--- math.js
+++ math.js
@@ -3,5 +3,22 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    return Number(a) + Number(b);
+    var a = parseNum(a);
+    var b = parseNum(b);
+
+    if (!a || !b) {
+        return false;
+    }
+
+    return a + b;
 };
+
+function parseNum(n) {
+    var n = +n;
+
+    if (n == NaN) {
+        return false;
+    }
+
+    return n;
+}
