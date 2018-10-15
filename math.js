1b56a59b6c39ccfb3c787c9aa1bd35411e0c5b83
hukopo@yandex.ru <opokuH1998>
2018-10-15 23:47:56 +0500

    q

--- math.js
+++ math.js
@@ -2,10 +2,6 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b, c) {
-    if (!Number.isInteger(a) || !Number.isInteger(b) || (c !== undefined && !Number.isInteger(c))) {
-        throw new TypeError();
-    }
-
-    return c === undefined ? a + b : a + b + c;
+exports.sum = function (a, b) {
+    return Number.parseFloat(a) + Number.parseFloat(b);
 };
