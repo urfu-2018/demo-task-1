6402d10bfc908657bc3986f980398b794eb747f1
Akishkina Veronika <nika_akishkina@mail.ru>
2018-10-15 05:23:35 +0500

    Пробую 3 переменные у index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const { sum } = require('./math');
 
-console.info(sum(1, 2));
+console.info(sum(1, 2, 1));
 // 3
 
-console.info(sum(63, 15));
+console.info(sum(63, 15, 1));
 // 78
 
-console.info(sum('12', 7));
+console.info(sum(12, 7, 1));
 // 19
