eb39436111589c2ca075157207612fe07434f2a9
Akishkina Veronika <nika_akishkina@mail.ru>
2018-10-15 05:27:07 +0500

    Попробую на 2 переменные с исправленной цифрой 12

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const { sum } = require('./math');
 
-console.info(sum(1, 2, 1));
+console.info(sum(1, 2));
 // 3
 
-console.info(sum(63, 15, 1));
+console.info(sum(63, 15));
 // 78
 
-console.info(sum(12, 7, 1));
+console.info(sum(12, 7));
 // 19
