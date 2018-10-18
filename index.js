df90b3607eeda8fe14d21854619ebd4383261260
Akishkina Veronika <nika_akishkina@mail.ru>
2018-10-19 00:26:32 +0500

    1

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const { sum } = require('./math');
 
 console.info(sum(1, 2));
-// 4
+// 3
 
 console.info(sum(63, 15));
-// 79
+// 78
 
-console.info(sum(12, 7));
+console.info(sum('12', 7));
+// 19
+
+console.info(sumThree('12', 7, 1));
 // 20
--- math.js
+++ math.js
@@ -3,8 +3,13 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-
-    const sum = Number(a) + Number(b);
-
-    return sum;
+    let sum = 0;
+    if (typeof(a)==='number' && typeof(b)==='number') {
+        sum = a + b;
+        return sum;
+    } else if (typeof(a)==='number' || typeof(b)==='number') {
+        sum = Number(a) + Number(b);
+        return sum;
+    } else return;
 };
+
