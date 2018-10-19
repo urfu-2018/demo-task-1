3c11402c100e9a236f3fc8e2f1292a010889f5ac
Akishkina Veronika <nika_akishkina@mail.ru>
2018-10-19 08:41:11 +0500

    5

--- math.js
+++ math.js
@@ -3,11 +3,6 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    const sum = Number(a) + Number(b);
-    if (typeof(sum) !== 'number' || sum === 'NaN') {
-        return 'undefined';
-    }
-
-    return sum;
+    return Number(a) + Number(b);
 };
 
